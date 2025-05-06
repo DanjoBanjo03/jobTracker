a1ae334e25f6dafb01f7a6f3d5d483081bbef723
danjobanjo03 <bajenovdan@gmail.com>
2025-05-06 10:03:49 -0400

    Made it run locally only, the tracker app was glitching

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template, redirect, url_for, request
-from flask_sqlalchemy import SQLAlchemy
-from projects.job_tracker.models import db, JobApplication
-from projects.job_tracker.forms import JobForm
+from jobTracker.models import db, JobApplication
+from jobTracker.forms import JobForm
 from datetime import date
-import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev-key')
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///jobs.db'
+app.config['SECRET_KEY'] = 'change-me'
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def index():
